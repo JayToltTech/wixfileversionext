e28656529b3baaded65340dce9d6157fdf2345ff
Jay Beavers <jay@tolttechnologies.com>
2022-06-08 16:47:15 -0700

    Nits

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.Tools.WindowsInstallerXml;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WixFileVersionExtension")]
+[assembly: AssemblyDefaultWixExtension(typeof(WixFileVersionExtension.WixFileVersionExtension))]
--- WixFileVersionPreprocessorExtension.cs
+++ WixFileVersionPreprocessorExtension.cs
@@ -21,26 +21,20 @@ namespace WixFileVersionExtension
             switch (prefix)
             {
                 case "fileVersion":
-                    // Make sure there actually is a file name
                     if (args.Length == 0 || args[0].Length == 0)
                         throw new ArgumentException("File name not specified");
 
-                    // Make sure the file exists
                     if (!File.Exists(args[0]))
                         throw new ArgumentException(string.Format("File name {0} does not exist", args[0]));
 
-                    // Get the file version information for the given file
                     FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(args[0]);
-
-                    // Get the property that matches the name of the function
                     PropertyInfo propertyInfo = fileVersionInfo.GetType().GetProperty(function);
 
-                    // Make sure the property exists
                     if (propertyInfo == null)
                         throw new ArgumentException(string.Format("Unable to find property {0} in FileVersionInfo", function));
 
-                    // Return the value of the property as a string
-                    return propertyInfo.GetValue(fileVersionInfo, null).ToString();
+                    var value = propertyInfo.GetValue(fileVersionInfo, null);
+                    return value.ToString(); ;
             }
 
             return null;
